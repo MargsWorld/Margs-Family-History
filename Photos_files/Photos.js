df619b0aa3c795814b8d2f61bdbfcf81b2f68dd8
Danny <43259125+parise22@users.noreply.github.com>
2023-07-14 23:44:59 +1000

    removing legacy links

--- Photos_files/Photos.js
+++ Photos_files/Photos.js
@@ -1,12 +1,12 @@
 // Created by iWeb 3.0.4 local-build-20131116
 
 function createMediaStream_id2()
-{return IWCreatePhotocast("http://members.iinet.net.au/%7Eembrace%40westnet.com.au/Margs_Family_History/index.html/Margs_Family_History/Photos_files/rss.xml",true);}
+{return IWCreatePhotocast("rss.xml",true);}
 function initializeMediaStream_id2()
-{createMediaStream_id2().load('http://members.iinet.net.au/%7Eembrace%40westnet.com.au/Margs_Family_History/index.html/Margs_Family_History',function(imageStream)
+{createMediaStream_id2().load('/',function(imageStream)
 {var entryCount=imageStream.length;var headerView=widgets['widget0'];headerView.setPreferenceForKey(imageStream.length,'entryCount');NotificationCenter.postNotification(new IWNotification('SetPage','id2',{pageIndex:0}));});}
 function layoutMediaGrid_id2(range)
-{createMediaStream_id2().load('http://members.iinet.net.au/%7Eembrace%40westnet.com.au/Margs_Family_History/index.html/Margs_Family_History',function(imageStream)
+{createMediaStream_id2().load('/',function(imageStream)
 {if(range==null)
 {range=new IWRange(0,imageStream.length);}
 IWLayoutPhotoGrid('id2',new IWPhotoGridLayout(3,new IWSize(182,182),new IWSize(182,57),new IWSize(218,254),27,27,0,new IWSize(18,18)),new IWPhotoFrame([IWCreateImage('Photos_files/Hardcover_bevel_01.png'),IWCreateImage('Photos_files/Hardcover_bevel_02.png'),IWCreateImage('Photos_files/Hardcover_bevel_03.png'),IWCreateImage('Photos_files/Hardcover_bevel_06.png'),IWCreateImage('Photos_files/Hardcover_bevel_09.png'),IWCreateImage('Photos_files/Hardcover_bevel_08.png'),IWCreateImage('Photos_files/Hardcover_bevel_07.png'),IWCreateImage('Photos_files/Hardcover_bevel_04.png')],null,0,0.500000,0.000000,0.000000,0.000000,0.000000,17.000000,17.000000,17.000000,17.000000,403.000000,295.000000,403.000000,295.000000,null,null,null,0.100000),imageStream,range,null,null,1.000000,{backgroundColor:'rgb(0, 0, 0)',reflectionHeight:100,reflectionOffset:2,captionHeight:100,fullScreen:0,transitionIndex:2},'Media/slideshow.html','widget0','widget1','widget2')});}
